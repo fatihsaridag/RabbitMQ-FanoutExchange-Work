9c66c6bbce2bc2f887bd780de090cb976b4871f2
Fatih Sarıdağ <68101192+fatihsaridag@users.noreply.github.com>
2022-12-20 12:16:42 +0300

    RabbitMQ Fanout Exchange Çalışması

---
+++ RabbitmqFirstProject/RabbitmqFirstProject.publisher/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace RabbitmqFirstProject.publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //RabbitMq ya bağlanmamız için connection factory isminde bir nesne örneğği alalım
+            var factory = new ConnectionFactory();
+            //Uri yımızı belirticez. CloudAmqp deki instancedan urli alıyoruz. Gerçek seneryoda bunları appsetting.json içeçrisinde tutuyoruz.
+            factory.Uri = new Uri("amqps://eznbdupx:Qf4h0Avxf0yEipy5VaR1D7UHRfIL0Gfn@gerbil.rmq.cloudamqp.com/eznbdupx");
+
+            //factory üzerinden bir bağlantı açıyoruz.
+            using var connection = factory.CreateConnection();
+            //Artık bir bağlantımız var ve bu bağlantı  üzerinden kanal oluşturuyoruz onun üzerinden bağlanıcaz.
+            var channel = connection.CreateModel(); //Bu kanal üzerinden rabbitMq ile haberleşebiliriz.
+            //1.param : Exchange ismi , 2.param : restart atınca uygulama fiziksel olarak kaydedilsin , 3.param : Exchange tipi ? 
+            channel.ExchangeDeclare("logs-fanout",durable:true, type:ExchangeType.Fanout);                                                        
+           
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+             {
+                 string log = $"log {x}"; // Byte çevirip istediğimiz formatı gönderebiliriz.
+                var messageBody = Encoding.UTF8.GetBytes(log);
+                 channel.BasicPublish("logs-fanout", "", null, messageBody);  
+                Console.WriteLine($"Mesaj Gönderilmiştir : {log}");
+             });
+
+           
+               
+        }
+    }
+}
---
+++ RabbitmqFirstProject/RabbitmqFirstProject.subscriber/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitmqFirstProject.subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            var factory = new ConnectionFactory(); 
+            factory.Uri = new Uri("amqps://eznbdupx:Qf4h0Avxf0yEipy5VaR1D7UHRfIL0Gfn@gerbil.rmq.cloudamqp.com/eznbdupx");  
+            using var connection = factory.CreateConnection(); 
+            var channel = connection.CreateModel();
+
+
+            //Kuyruk oluşturucaz ve bu kuyrugun kendisine ait Instance'ı olsun bu yüzden random bir kuyruk oluşturuyoruz.
+            var randomQueue = channel.QueueDeclare().QueueName;                  //Bu bize random bir kuruk ismi veriyor.
+
+            channel.QueueBind(randomQueue, "logs-fanout", "", null);              //QueBind ile beraber uygulama her ayağa kalktığında bir kuyruk oluşacak ve uygulama down olunca kuyruk silinecek.
+                                                                                 //Eğer QueueDeclare deseydik ilgili subscriber kapansa bile kuyruk durur (Yanlış kullanım bu seneryoda)
+
+
+
+
+            channel.BasicQos(0, 1, false);                                       // Birer birer alsın diyoruz.  
+            var consumer = new EventingBasicConsumer(channel);        
+            channel.BasicConsume(randomQueue, false,consumer);                   // Yukarıdaki random kuyruk ismini tüketmesini istiyoruz
+            Console.WriteLine("Loglar dinleniyor..");                            // Bu yazıyı almaya çalışalım.
+            consumer.Received += (object sender, BasicDeliverEventArgs e) => {  
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Thread.Sleep(1500);   
+                Console.WriteLine("Gelen mesaj : " + message);
+
+                channel.BasicAck(e.DeliveryTag, false); 
+            };
+            Console.ReadLine();
+        }
+    }
+}
